fbd414ccb1cb8ee5275b17df5cf59175a16b1e04
Carlos Vitor <carlosfdm@outlook.com>
2022-09-23 17:25:42 -0300

    .

--- Lista2_PROG_I/03/3.c
+++ Lista2_PROG_I/03/3.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main (){
+int main() {
+
+    int i, j, mat1[3][3] = {11,12,13,14,15,16,17,18,19};
 
     srand(time(NULL));
 
-    int i;
-    int n [10];
-
-    for(i=0;i<=9;i++){
-
-        n[i] = (rand() % 11) + 10;
-
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            mat1[i][j] = rand() % 6;
+        }
     }
 
-    for(i=0;i<=9;i++){
-
-        printf("n[%d] = %d\n", i, n[i]);
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            printf("%5d ", mat1[i][j]);
+        printf("\n");
     }
+
+    printf("\n--> %d\n", RAND_MAX);
 
     return 0;
 }
--- Lista2_PROG_I/07/7.c
+++ Lista2_PROG_I/07/7.c
@@ -8,22 +8,64 @@
 
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(){
 
+    setlocale(LC_ALL, "Portuguese");
+
+    int m[3][6], i, j, num;
+
+    srand(time(NULL));
+
+    for(i=0;i<3;i++){
+
+        for(j=0;j<6;j++){
+
+            m[i][j] = rand() % 20;
+        }
+    }
+
+    printf("Matriz 3x6 com números de 0 a 20 aleátorios\n\n");
+
+    printf("========================\n");
+    for(i=0;i<3;i++){
+
+        for(j=0;j<6;j++){
+
+            printf("%3d ", m[i][j]);
+        }
+
+        printf("\n");
+    }
+    printf("========================\n\n");
+
+    printf("Digite um número para multiplicar todos os elementos da matriz acima: ");
+    scanf("%d", &num);
+
+    for(i=0;i<3;i++){
+
+        for(j=0;j<6;j++){
+
+            m[i][j] = num * m[i][j];
+        }
+    }
 
 
+    printf("\nResultado da Matriz após a multiplicação de todos os elementos por %d\n\n", num);
+    printf("========================\n");
+    for(i=0;i<3;i++){
 
+        for(j=0;j<6;j++){
 
+            printf("%3d ", m[i][j]);
+        }
 
-
-
-
-
-
-
+        printf("\n");
+    }
+    printf("========================\n");
 
 
     return 0;
 }
-
